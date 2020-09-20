68b9834fa467d09772d81a9a1f3b4366ae3738ce
Timofey <timaluk2icloud.com>
2020-09-20 19:09:56 +0300

    feat: add demo UI for second module

--- WeatherApp/WeekModule/Views/WeekForecastViewController.swift
+++ WeatherApp/WeekModule/Views/WeekForecastViewController.swift
@@ -16,15 +16,35 @@ class WeekForecastViewController: UIViewController, UITableViewDelegate, UITable
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationItem.title = "Forecast"
+        self.view.backgroundColor = .white
+        
+        forecastTable = UITableView()
+        forecastTable.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(forecastTable)
+        forecastTable.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        forecastTable.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        forecastTable.widthAnchor.constraint(equalTo: view.widthAnchor).isActive = true
     }
     
     
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return presenter.forecast?.data?.keys.count ?? 0
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        
+        if let forecast = presenter.forecast?.data{
+            let days = Array(forecast.keys).map({ String($0) })
+            let key = days[section]
+            
+            return forecast[key]??.count ?? 0
+        }
+        return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        let cell = tableView.dequeueReusableCell(withIdentifier: "forecastCell", for: indexPath)
+        return cell
     }
     
     
